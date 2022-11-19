2a686a458af17fe9a11ee4da13c4807a0af7c75d
Ernest <ernestl8502@gmail.com>
2022-11-19 11:53:54 -0800

    Finished lab 8

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # Lab 8 - Starter
+
+1) Within a github action that runs whenever code is pushed, since it would ensure that all code in the repository will function together without issues and keep the repository clean of bugs
+2) No
+3) No, since that feature requires multiple components interacting with each other, which is the opposite of what unit testing does
+4) Yes, since all that is needed for input is a string of the message to be used and it can be done without interacting with other components
--- __tests__/unit.test.js
+++ __tests__/unit.test.js
@@ -3,3 +3,87 @@
 const functions = require('../code-to-unit-test/unit-test-me.js');
 
 // TODO - Part 2
+test("Test a valid phone number 1", () => {
+    expect(functions.isPhoneNumber("(123) 569-1948")).toBe(true);
+});
+
+test("Test a valid phone number 2", () => {
+    expect(functions.isPhoneNumber("(582) 111-0000")).toBe(true);
+});
+
+test("Test an invalid phone number", () => {
+    expect(functions.isPhoneNumber("12")).toBe(false);
+});
+
+test("Test an invalid phone number", () => {
+    expect(functions.isPhoneNumber("1209348548")).toBe(false);
+});
+
+test("Test valid email 1", () => {
+    expect(functions.isEmail("eaoidjf@ucsd.edu")).toBe(true);
+});
+
+test("Test valid email 2", () => {
+    expect(functions.isEmail("138djhu1919j@target.com")).toBe(true);
+});
+
+test("Test invalid email 1", () => {
+    expect(functions.isEmail("bob@g")).toBe(false);
+});
+
+test("Test invalid email 2", () => {
+    expect(functions.isEmail("@gadfm.com")).toBe(false);
+});
+
+/**
+ * The password's first character must be a letter, it must contain at least * 4 
+ * characters and no more than 15 characters and no characters other than * * letters, 
+ * numbers and the underscore may be used
+ */
+test("Test strong password 1", () => {
+    expect(functions.isStrongPassword("Ajidfo103")).toBe(true);
+});
+
+test("Test strong password 2", () => {
+    expect(functions.isStrongPassword("A____dfadf")).toBe(true);
+});
+
+test("Test weak password 1", () => {
+    expect(functions.isStrongPassword("1234")).toBe(false);
+});
+
+test("Test weak password 2", () => {
+    expect(functions.isStrongPassword("Adfaadkakjfjkdfaojdfaiojdfaiojdoifjoa")).toBe(false);
+});
+
+test("Valid date 11/12/1111", () => {
+    expect(functions.isDate("11/12/1111")).toBe(true);
+});
+
+test("Valid date 1 / 1 / 2022", () => {
+    expect(functions.isDate("1/1/2022")).toBe(true);
+});
+
+test("Invalid date 111/1/2022", () => {
+    expect(functions.isDate("111/1/2022")).toBe(false);
+});
+
+test("Invalid date 1/1/203023", () => {
+    expect(functions.isDate("1/1/203023")).toBe(false);
+});
+
+test("Testing hex #AAA", () => {
+    expect(functions.isHexColor("#AAA")).toBe(true);
+});
+
+test("Testing hex #22F", () => {
+    expect(functions.isHexColor("#22F")).toBe(true);
+});
+
+test("Testing invalid hex 2", () => {
+    expect(functions.isHexColor("2")).toBe(false);
+});
+
+test("Testing invalid hex #AAAAAAAAAAA", () => {
+    expect(functions.isHexColor("#AAAAAAAAAAA")).toBe(false);
+});
